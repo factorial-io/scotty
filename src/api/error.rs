5cf3f281670266b64d3d51b091bf77b00a2985cc
Stephan Huber <stephan@factorial.io>
2024-08-27 10:45:09 +0200

    chore: Cleanup error handling code

--- src/api/error.rs
+++ src/api/error.rs
@@ -8,7 +8,7 @@ use axum::{
 use thiserror::Error;
 use uuid::Uuid;
 
-#[derive(Error, Debug, utoipa::ToResponse)]
+#[derive(Clone, Error, Debug, utoipa::ToResponse)]
 pub enum AppError {
     #[error("Service unavailable")]
     ServiceUnavailable,
@@ -16,22 +16,22 @@ pub enum AppError {
     #[error("Not found")]
     NotFound,
 
-    #[error("Internal server error")]
+    #[error("Internal server error: {0}")]
     InternalServerError(String),
 
     #[error("Invalid input")]
     InvalidInput,
 
-    #[error("App not found")]
+    #[error("App not found: {0}")]
     AppNotFound(String),
 
-    #[error("Task not found")]
+    #[error("Task not found: {0}")]
     TaskNotFound(Uuid),
 
     #[error("File content could not be decoded!")]
     FileContentDecodingError,
 
-    #[error("Cant destroy an unmanaged app!")]
+    #[error("Cant destroy an unmanaged app: {0}")]
     CantDestroyUnmanagedApp(String),
 
     #[error("Missing docker-compose file in the payload")]
@@ -39,57 +39,39 @@ pub enum AppError {
     #[error("Invalid docker-compose file")]
     InvalidDockerComposeFile,
 
-    #[error("Service not found in docker compose file!")]
+    #[error("Service not found in docker compose file: {0}")]
     PublicServiceNotFound(String),
+
+    #[error("Registry not found: {0}")]
+    RegistryNotFound(String),
+}
+impl AppError {
+    fn get_error_msg(&self) -> (axum::http::StatusCode, String) {
+        let status: axum::http::StatusCode = match self {
+            AppError::ServiceUnavailable => StatusCode::SERVICE_UNAVAILABLE,
+            AppError::NotFound => StatusCode::NOT_FOUND,
+            AppError::InternalServerError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            AppError::AppNotFound(_) => StatusCode::NOT_FOUND,
+            AppError::TaskNotFound(_) => StatusCode::NOT_FOUND,
+            _ => StatusCode::BAD_REQUEST,
+        };
+
+        (status, self.to_string())
+    }
 }
 
 impl From<anyhow::Error> for AppError {
     fn from(e: anyhow::Error) -> Self {
+        if let Some(app_error) = e.downcast_ref::<AppError>() {
+            return app_error.clone();
+        }
         AppError::InternalServerError(e.to_string())
     }
 }
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        let (status, body): (axum::http::StatusCode, String) = match self {
-            AppError::ServiceUnavailable => (
-                StatusCode::SERVICE_UNAVAILABLE,
-                "Service unavailable".into(),
-            ),
-            AppError::NotFound => (StatusCode::NOT_FOUND, "Resource not found".into()),
-            AppError::InternalServerError(msg) => (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                format!("Internal server error: {}", &msg),
-            ),
-            AppError::InvalidInput => (StatusCode::BAD_REQUEST, "Invalid input".into()),
-            AppError::AppNotFound(app_id) => {
-                (StatusCode::NOT_FOUND, format!("App not found: {}", app_id))
-            }
-            AppError::TaskNotFound(task_uuid) => (
-                StatusCode::NOT_FOUND,
-                format!("Task not found: {}", task_uuid),
-            ),
-            AppError::FileContentDecodingError => (
-                StatusCode::BAD_REQUEST,
-                "File content could not be decoded!".into(),
-            ),
-            AppError::CantDestroyUnmanagedApp(app_id) => (
-                StatusCode::BAD_REQUEST,
-                format!("Cant destroy app {} as it is not managed by us!", app_id),
-            ),
-            AppError::NoDockerComposeFile => (
-                StatusCode::BAD_REQUEST,
-                "Missing docker-compose file in the payload".into(),
-            ),
-            AppError::InvalidDockerComposeFile => (
-                StatusCode::BAD_REQUEST,
-                "Invalid docker-compose file".into(),
-            ),
-            AppError::PublicServiceNotFound(service_name) => (
-                StatusCode::BAD_REQUEST,
-                format!("Service not found in docker compose file: {}", service_name),
-            ),
-        };
+        let (status, body) = self.get_error_msg();
         let body = serde_json::json!({ "error": true, "message": body });
         (status, Json(body)).into_response()
     }
