e59d2126c230bfe9fc0733e4b9b41734c5848454
Stephan Huber <stephan@factorial.io>
2024-09-19 18:36:26 +0200

    feat: Start wiht frontend

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+	import 'tailwindcss/tailwind.css';
+	import logo from '$lib/assets/scotty.svg';
+</script>
+
+<div class="m-4 p-4 border-2 border-gray-100 rounded">
+	<div class="navbar bg-base-100 border-b-2 border-gray-100">
+		<div class="flex-1">
+			<div class="avatar">
+				<div class="w-16 rounded-full bg-gray-50">
+					<img alt="Scotty Logo" src={logo} />
+				</div>
+			</div>
+			<a href="/" class="btn btn-ghost text-xl">scotty</a>
+		</div>
+		<div class="flex-none">
+			<ul class="menu menu-horizontal px-1">
+				<li><a href="/rapidocs">API Documentation</a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="m-4 p-4">
+		<slot />
+	</div>
+</div>
