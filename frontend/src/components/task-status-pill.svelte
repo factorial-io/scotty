2da842281dc766b1a0a719dba7b8db12d7696044
Stephan Huber <stephan@factorial.io>
2024-09-23 11:58:00 +0200

    fix: Show better task results

---
+++ frontend/src/components/task-status-pill.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let status = 'Running';
+
+	$: statusColor =
+		status === 'Failed'
+			? 'bg-red-500'
+			: status === 'Finished'
+				? 'bg-green-500'
+				: status === 'Running'
+					? 'bg-secondary'
+					: 'bg-gray-300';
+</script>
+
+<span class="text-white px-3 py-1 rounded-full text-xs {statusColor}">{status}</span>
