afbd458883357e55195627eda290c5d969459a63
Stephan Huber <stephan@factorial.io>
2025-05-06 12:06:24 +0200

    feat(frontend): enhance environment-variable display and add defaults
    Improve the readability and maintainability of environment variables by sorting them alphabetically in the UI and changing the CSS for better wrapping. Additionally, demo values are added to one of the apps to help debugging.

--- frontend/src/components/format-environment-variables.svelte
+++ frontend/src/components/format-environment-variables.svelte
@@ -6,11 +6,20 @@
 	None
 {:else}
 	<dl class="flex flex-col gap-1">
-		{#each Object.entries(environment) as [key, value] (key)}
+		{#each Object.entries(environment).sort(([a], [b]) => a.localeCompare(b)) as [key, value] (key)}
 			<div class="flex">
-				<dt class="font-mono">{key}=</dt>
-				<dd class="font-mono break-all">{value}</dd>
+				<dt class="font-mono">{key}&nbsp;=&nbsp;</dt>
+				<dd class="font-mono wrap-anywhere">{value}</dd>
 			</div>
 		{/each}
 	</dl>
 {/if}
+
+<style>
+	.wrap-anywhere {
+		word-break: break-all;
+		overflow-wrap: anywhere;
+		white-space: pre-wrap;
+		max-width: 100%;
+	}
+</style>
